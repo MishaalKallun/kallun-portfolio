9700735f8fac345c484b3cea2f575b1650effc76
Guest User <guestuser@Guests-MacBook-Pro.local>
2021-10-27 21:08:12 +0800

    tried to make at look atleast somewhat decent...

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,18 @@ import React from 'react'
 
 // Bootstrap Components
 import ListGroup from 'react-bootstrap/ListGroup'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 export default function About(){
 	return(
-		<ListGroup>
-			<ListGroup.Item>I like cheese</ListGroup.Item>
-			<ListGroup.Item>I like card games</ListGroup.Item>
-			<ListGroup.Item>I like cactuses</ListGroup.Item>
-		</ListGroup>
+		<Jumbotron>
+		  <h1>About me!</h1>
+		  <ListGroup>
+		  	<ListGroup.Item>I like cheese</ListGroup.Item>
+		  	<ListGroup.Item>I like card games</ListGroup.Item>
+		  	<ListGroup.Item>I like cactuses</ListGroup.Item>
+		  </ListGroup>
+		</Jumbotron>
+		
 	)
 }
