8fa5c30b2f4f4fa6e86ee27a316124799b9ce0c5
Guest User <guestuser@Guests-MacBook-Pro.local>
2021-10-27 20:58:32 +0800

    added simple about me

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+// React Library
+import React from 'react'
+
+// Bootstrap Components
+import ListGroup from 'react-bootstrap/ListGroup'
+
+export default function About(){
+	return(
+		<ListGroup>
+			<ListGroup.Item>I like cheese</ListGroup.Item>
+			<ListGroup.Item>I like card games</ListGroup.Item>
+			<ListGroup.Item>I like cactuses</ListGroup.Item>
+		</ListGroup>
+	)
+}
